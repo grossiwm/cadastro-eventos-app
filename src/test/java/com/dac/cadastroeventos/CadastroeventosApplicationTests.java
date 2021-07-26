ce3072fd6264ab2072d30fadfdea3034ddba977e
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2021-07-26 19:15:25 -0300

    commit inicial

---
+++ src/test/java/com/dac/cadastroeventos/CadastroeventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dac.cadastroeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroeventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
